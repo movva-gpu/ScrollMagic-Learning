0b1ed7e3674fa5acb93e2bb6d5a4a7ba84d16591
Movva <81530181+movva-gpu@users.noreply.github.com>
2024-02-12 23:28:24 +0100

    feat:each() usage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,11 @@
 $(document).ready(() => {
     var controller = new ScrollMagic.Controller();
 
-    new ScrollMagic.Scene({
-        triggerElement: '.sec2',
-        offset: $('.sec2').height() / -2,
-        duration: $('.sec2').height() * 2
-    }).setClassToggle('.sec2', 'slide-in').addIndicators().addTo(controller);
-
-    new ScrollMagic.Scene({
-        triggerElement: '.sec3',
-        offset: $('.sec3').height() / -2,
-        duration: $('.sec3').height() * 2
-    }).setClassToggle('.sec3', 'slide-in').addTo(controller);
-
-    console.log(controller);
+    $('.smaller').each(i => {
+        new ScrollMagic.Scene({
+            triggerElement: $('.smaller')[i],
+            offset: '-200%',
+            duration: '1    00%'
+        }).setClassToggle($('.smaller')[i],'slide-in').addIndicators().addTo(controller);
+    })
 });
