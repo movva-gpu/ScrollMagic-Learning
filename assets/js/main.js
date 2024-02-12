c350e16eabd970a349bfa19fb9a60e723166b819
Movva <81530181+movva-gpu@users.noreply.github.com>
2024-02-13 00:10:25 +0100

    feat:More scenes and my logo
    Added a scene on all elements of .header
    
    Added a logo in a <header> element to my GitHub

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,47 @@
 $(document).ready(() => {
     var controller = new ScrollMagic.Controller();
 
+    new ScrollMagic.Scene({
+        triggerElement: '.header',
+        triggerHook: 0,
+        duration: '100%'
+    })
+        .setPin('.header', { pushFollowers: false })
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+        triggerElement: '.sec2',
+        offset: '-150%',
+    })
+        .setClassToggle('.hat', 'up')
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+        triggerElement: '.sec2',
+        offset: '-150%',
+    })
+        .setClassToggle('.header h2', 'right')
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+        triggerElement: '.sec2',
+        offset: '-150%',
+    })
+        .setClassToggle('.header p', 'shrink')
+        .addTo(controller);
+
     $('.smaller').each(i => {
         new ScrollMagic.Scene({
             triggerElement: $('.smaller')[i],
             offset: '-200%',
-            duration: '1    00%'
-        }).setClassToggle($('.smaller')[i],'slide-in').addIndicators().addTo(controller);
+            duration: '100%'
+        })
+            .setClassToggle($('.smaller')[i],'slide-in')
+            .addTo(controller);
+    });
+
+    $(window).on('scroll', () => {
+        if ($(window).scrollTop() < $('.empty').position().top - $('header').outerHeight()) {$('.logo').css('fill', 'white'); return; }
+        $('.logo').css('fill', 'black');
     })
 });
