f1965f982f7d63ef71fa3c088d1b7175cb95a074
Movva <81530181+movva-gpu@users.noreply.github.com>
2024-02-13 14:39:03 +0100

    feat:Added a bunch of Tweens and a little nav menu
    Made most transitions use Tweens, even the opening of the nav
    
    Added a small nav with links to my GitHub, my Mastodon and the source

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,58 @@
 $(document).ready(() => {
     var controller = new ScrollMagic.Controller();
 
-    // #region .header
+    // #region Header Timeline
+
+    var headerTimeline = new TimelineMax()
+        .to('header details', 3, {
+            x: window.innerWidth - $('.logo').outerWidth() * 2 - 50,
+            ease: 'power1.inOut'
+        }, 0)
+        .to('.hat', 1, {
+            y: $(window).height() / -2,
+            ease: 'circle.in'
+        }, 1)
+        .to('.duck', 1, {
+            y: -1000,
+            rotate: '7200deg',
+            scale: '10% 10%',
+            ease: 'power1.in'
+        }, 1)
+        .to('.header h2', 1, {
+            x: $(window).width() - $('.header h2').outerWidth(),
+            ease: 'power1.in'
+        }, 1)
+        .to('.header p', 1, {
+            y: 100,
+            scale: '0 0',
+            ease: 'power1.in'
+        }, 1);
+
+    // #endregion
+
+    // #region Lower Sections Timeline
+
+    var sectionsTimeline = new TimelineMax()
+        .from('.sec2', 1, {
+            x: 1000,
+            ease: 'circle.out'
+        }, 0)
+        .from('.sec3', 1, {
+            x: -1000,
+            ease: 'circle.out'
+        }, .5);
+
+    // #endregion
+
+    // #region Header
+
+    new ScrollMagic.Scene({
+        triggerElement: '.header',
+        triggerHook: 0,
+        duration: '100%'
+    })
+        .setTween(headerTimeline)
+        .addTo(controller)
 
     new ScrollMagic.Scene({
         triggerElement: '.header',
@@ -11,57 +62,59 @@ $(document).ready(() => {
         .setPin('.header', { pushFollowers: false })
         .addTo(controller);
 
-    new ScrollMagic.Scene({
-        triggerElement: '.sec2',
-        offset: '-200%',
-    })
-        .setClassToggle('.hat', 'up')
-        .addTo(controller);
-
-    new ScrollMagic.Scene({
-        triggerElement: '.sec2',
-        offset: '-200%',
-    })
-        .setClassToggle('.header h2', 'right')
-        .addTo(controller);
-
-    new ScrollMagic.Scene({
-        triggerElement: '.sec2',
-        offset: '-200%',
-    })
-        .setClassToggle('.header p', 'shrink')
-        .addTo(controller);
-
-    new ScrollMagic.Scene({
-        triggerElement: '.sec2',
-        offset: '-200%',
-    })
-        .setClassToggle('.header .duck', 'bewm')
-        .addTo(controller);
-
     // #endregion
 
-    $('.smaller').each(i => {
-        new ScrollMagic.Scene({
-            triggerElement: $('.smaller')[i],
-            offset: '-250%',
-            duration: '100%'
-        })
-            .setClassToggle($('.smaller')[i],'slide-in')
-            .addTo(controller);
-    });
+    // #region Lower Sections
+
+    new ScrollMagic.Scene({
+        triggerElement: '.lower',
+        triggerHook: 1,
+        duration: $('.lower').height()
+    })
+        .setTween(sectionsTimeline)
+        .addTo(controller);
 
     new ScrollMagic.Scene({
         triggerElement: '.parallax',
-        triggerHook: 0,
-        offset: -800,
-        duration: 1600
+        triggerHook: 1,
+        duration: $('.parallax').height() + $(window).height()
     })
-        .setTween(TweenMax.from('.parallax img', 1, { y:'-30%', ease: Power0.easeNone }))
+        .setTween(TweenMax.from('.parallax img', 1, { y:'-50%', ease: 'none' }))
         .addTo(controller);
 
     $(window).on('scroll', () => {
-        if ($(window).scrollTop() < $('.empty').position().top - $('header').outerHeight()) {$('.logo').css('fill', 'white'); return; }
-        $('.logo').css('fill', 'black');
-    })
+        if ($(window).scrollTop() < $('.empty').position().top - $('header').outerHeight()) {$('header svg').css('fill', 'white'); return; }
+        $('header svg').css('fill', 'black');
+    });
+
+    $('.logo-sum').on('click', e => {
+        e.preventDefault();
+        $('header details').prop('open') ? closeLogo() : openLogo();
+    });
 });
+
+let opening = new TweenMax.from('header details li', 1, {
+    scale: 0,
+    ease: 'back.out',
+    paused: true
+});
+
+let closing = new TweenMax.to('header details li', 1, {
+    scale: 0,
+    ease: 'power3.inOut',
+    paused: true
+});
+
+function openLogo() {
+    if (!closing.isActive() && !opening.isActive()) {
+        $('header details').prop('open', true);
+        opening.play(0);
+    }
+}
+
+function closeLogo() {
+    if (!closing.isActive() && !opening.isActive()) {
+        closing.play(0);
+        setTimeout(() => { $('header details').prop('open', false), TweenMax.to('header details li', 0, { scale: 1 }) }, 1000);
+    }
+}
